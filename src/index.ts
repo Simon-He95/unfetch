943870087ebe07080906fd86b67fd6260aa71b2a
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-11-26 13:59:50 +0800

    chore: type

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import type { FetchOptions } from 'ofetch'
 import { ofetch } from 'ofetch'
 import type { ExtendedFetch, Request } from './types'
 
-function unfetch(defaults: FetchOptions) {
+function unfetch(defaults: FetchOptions = {}) {
   const apiFetch: ExtendedFetch = ofetch.create(defaults) as ExtendedFetch
   const getFetch: Request = (request, options) => {
     return apiFetch(request, {
@@ -11,31 +11,35 @@ function unfetch(defaults: FetchOptions) {
     })
   }
 
-  const postFetch: Request = (request, options) => apiFetch(request, {
-    method: 'POST',
-    ...options,
-  })
+  const postFetch: Request = (request, options) =>
+    apiFetch(request, {
+      method: 'POST',
+      ...options,
+    })
 
-  const putFetch: Request = (request, options) => apiFetch(request, {
-    method: 'PUT',
-    ...options,
-  })
+  const putFetch: Request = (request, options) =>
+    apiFetch(request, {
+      method: 'PUT',
+      ...options,
+    })
 
-  const deleteFetch: Request = (request, options) => apiFetch(request, {
-    method: 'DELETE',
-    ...options,
-  })
+  const deleteFetch: Request = (request, options) =>
+    apiFetch(request, {
+      method: 'DELETE',
+      ...options,
+    })
 
-  const patchFetch: Request = (request, options) => apiFetch(request, {
-    method: 'PATCH',
-    ...options,
-  });
+  const patchFetch: Request = (request, options) =>
+    apiFetch(request, {
+      method: 'PATCH',
+      ...options,
+    })
 
-  (apiFetch as ExtendedFetch).get = getFetch;
-  (apiFetch as ExtendedFetch).post = postFetch;
-  (apiFetch as ExtendedFetch).put = putFetch;
-  (apiFetch as ExtendedFetch).delete = deleteFetch;
-  (apiFetch as ExtendedFetch).patch = patchFetch
+  ;(apiFetch as ExtendedFetch).get = getFetch
+  ;(apiFetch as ExtendedFetch).post = postFetch
+  ;(apiFetch as ExtendedFetch).put = putFetch
+  ;(apiFetch as ExtendedFetch).delete = deleteFetch
+  ;(apiFetch as ExtendedFetch).patch = patchFetch
 
   return apiFetch
 }
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,15 @@
-import type { $Fetch, FetchOptions, FetchRequest, MappedResponseType, ResponseType } from 'ofetch'
+import type {
+  $Fetch,
+  FetchOptions,
+  FetchRequest,
+  MappedResponseType,
+  ResponseType,
+} from 'ofetch'
 
-export type Request = <T = any, R extends ResponseType = 'json'>(request: FetchRequest, options?: FetchOptions<R>) => Promise<MappedResponseType<R, T>>
+export type Request = <T = any, R extends ResponseType = 'json'>(
+  request: FetchRequest,
+  options?: FetchOptions<R> & Record<string, any>,
+) => Promise<MappedResponseType<R, T>>
 export interface ExtendedFetch extends $Fetch {
   get: Request
   post: Request
